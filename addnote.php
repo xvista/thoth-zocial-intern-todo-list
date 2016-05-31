42f82cb595f086344c9913bd4a1ab55492fe4863
Pasit Niyomthong <enqueteplu@gmail.com>
2016-05-31 15:19:05 +0700

    upload img

--- addnote.php
+++ addnote.php
@@ -6,9 +6,10 @@
   </head>
   <body>
     <h3>Create Note</h3>
-    <form action="addnote_process.php" method="post">
+    <form action="addnote_process.php" method="post" enctype="multipart/form-data">
     title <input type="text" name="title"><br>
     content <input type="text" name="content"><br>
+    Image <input type="file" name="image"><br>
     Important<br>
     YES <input type="radio" name="important" value="1">
     NO <input type="radio" name="important" value="0"><br>
--- addnote_process.php
+++ addnote_process.php
@@ -8,7 +8,29 @@ if(isset($_POST["title"],$_POST["content"],$_POST["important"],$_POST["type"])){
   $content = $_POST["content"];
   $type = $_POST["type"];
   $impo = $_POST["important"];
-  $sql = "INSERT INTO note (note_id,title,content,img,created_at,type,important) VALUES (NULL,'$title','$content','',CURRENT_TIMESTAMP,$type,$impo)";
+  $file_url = "";
+    if(isset($_FILES["image"])){
+      $errors = array();
+      $file_name = $_FILES["image"]["name"];
+      $file_size = $_FILES["image"]["size"];
+      $file_tmp = $_FILES["image"]["tmp_name"];
+      $file_type = $_FILES["image"]["type"];
+      $file_ext = strtolower(end(explode('.',$_FILES["image"]["name"])));
+
+      $expention = array("jpg","png");
+
+      if(in_array($file_ext,$expention)===false){
+        $errors[] = "File isn't to use jpg or png";
+      }
+      if(empty($errors)==true){
+        $file_url = "img/".$file_name;
+        move_uploaded_file($file_tmp,"img/".$file_name);
+      }else{
+        echo "error to upload";
+      }
+    }
+
+  $sql = "INSERT INTO note (note_id,title,content,img,created_at,type,important) VALUES (NULL,'$title','$content','$file_url',CURRENT_TIMESTAMP,$type,$impo)";
   echo $sql;
   mysqli_query($connect,$sql);
 }
