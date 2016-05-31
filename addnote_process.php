32b84886527c5c421cac04d21fbee6b2d582e11e
Atikhun Orisoon <genatorster@gmail.com>
2016-05-31 14:43:39 +0700

    Edit INSERT statement, using NULL in primary key

--- addnote_process.php
+++ addnote_process.php
@@ -8,7 +8,7 @@ if(isset($_POST["title"],$_POST["content"],$_POST["important"],$_POST["type"])){
   $content = $_POST["content"];
   $type = $_POST["type"];
   $impo = $_POST["important"];
-  $sql = "INSERT INTO note (note_id,title,content,img,created_at,type,important) VALUES ('','$title','$content','',CURRENT_TIMESTAMP,$type,$impo)";
+  $sql = "INSERT INTO note (note_id,title,content,img,created_at,type,important) VALUES (NULL,'$title','$content','',CURRENT_TIMESTAMP,$type,$impo)";
   echo $sql;
   mysqli_query($connect,$sql);
 }
--- checklist_process.php
+++ checklist_process.php
@@ -10,13 +10,13 @@ if(isset($_POST["ch"])){
   $content = $_POST["content"];
   $type = $_POST["type"];
   $impo = $_POST["important"];
-  $sql = "INSERT INTO note (note_id,title,content,img,created_at,type,important) VALUES ('','$title','$content','',CURRENT_TIMESTAMP,$type,$impo)";
+  $sql = "INSERT INTO note (note_id,title,content,img,created_at,type,important) VALUES (NULL,'$title','$content','',CURRENT_TIMESTAMP,$type,$impo)";
   mysqli_query($connect,$sql);
   $keep_id = mysqli_insert_id($connect);
 foreach ($checked as $check) {
   if($check!=""){
 
-      $sql_checklist = "INSERT INTO checklist(checklist_id,note_id,content,checked) VALUES ('','$keep_id','$check','0')";
+      $sql_checklist = "INSERT INTO checklist(checklist_id,note_id,content,checked) VALUES (NULL,'$keep_id','$check','0')";
       mysqli_query($connect,$sql_checklist);
   echo $check."<br>";
   }
